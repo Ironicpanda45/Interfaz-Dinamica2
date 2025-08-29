647e0027b787f09c8c9859507a78330dc9eff28e
Ironicpanda45 <Leiruzielrp@gmail.com>
2025-08-29 12:51:44 -0600

    [Actualiza]Actualize eñ css y el js

--- js/script.js
+++ js/script.js
@@ -13,12 +13,25 @@ function crearimagen(event) {
     imagen.classList.add("imagen-nueva");
     elementos.appendChild(imagen);
     */
-    const imagen = new Image();
-    const texto = document.createElement("h2");
-    imagen.src = "../resources/img/greninja.png";
-    texto.textContent = imagen.src.substring(imagen.src.lastIndexOf("/")+1);
+    const tarjetacontenedor = document.createElement("div");
+    tarjetacontenedor.classList.add("tarjetacontenedor");
+    elementos.appendChild(tarjetacontenedor);
+
+    const imagen = document.createElement("img");
+    tarjetacontenedor.appendChild(imagen);
     imagen.classList.add("imagen-nueva");
-    elementos.appendChild(imagen);
-    elementos.appendChild(texto);
+    imagen.src = "../resources/img/greninja.png";
+
+    const texto = document.createElement("h2");
+    tarjetacontenedor.appendChild(texto);
+    texto.textContent = imagen.src.substring(imagen.src.lastIndexOf("/")+1);
+
+    const parrafo = document.createElement("p");
+    tarjetacontenedor.appendChild(parrafo);
+    parrafo.textContent = "Esto es un texto de ejemplo"
+
+    const boton = document.createElement("button");
+    tarjetacontenedor.appendChild(boton);
+    boton.textContent = " Mostrar mas..."
     }
 }
