50284fd3172acd1f516ae832c1d7514692975491
Soyiro <Leiruzielrp@gmail.com>
2025-09-08 12:27:53 -0600

    Agrega

--- js/formulario.js
+++ js/formulario.js
@@ -1,14 +1,18 @@
 const formulario_producto_nuevo = document.querySelector("#formulario_producto");
+let Productos = [];
+let src_imagen_producto = "./resources/img/1077596-200.png";
 
 class Producto{
-    constructor(id, nombre, descripcion, precio){
+    constructor(id, imagen, nombre, descripcion, precio){
         this.id = id;
+        this.imagen = imagen;
         this.nombre = nombre;
         this.descripcion = descripcion;
         this.precio = precio;
     }
     Obtenerdatos(){
         console.log(this.id);
+        console.log(this.imagen);
         console.log(this.nombre);
         console.log(this.descripcion)
         console.log(this.precio)
@@ -18,6 +22,23 @@ class Producto{
 function AgregarProducto(event){
     let lectorformulario = new FormData(formulario_producto_nuevo);
     const datos = Object.fromEntries(lectorformulario.entries());
-    const json = JSON.stringify(datos);
-    console.log(json);
+    if(datos.nombre !="" && datos.descripcion !="" && datos.precio!=null){
+    Productos.push(new Producto(Productos.length+1, src_imagen_producto,datos.nombre, datos.descripcion, datos.precio));
+    Productos.forEach(producto => {
+        producto.Obtenerdatos();
+    });
+        }
+    //const json = JSON.stringify(datos);
+    //console.log(json);
+}
+function ObtenerImagen(event){
+    const file = event.target.files[0];
+    if (file.file === " image/jpeg" || file.type === "image/png"){
+        const lector = new FileReader();
+        lector.onload = (event) => {
+            src_imagen_producto = event.target.result
+            document.querySelector("#imagen-file").src = src_imagen_producto;
+        }
+        lector.readAsDataURL(file);
+    }
 }
