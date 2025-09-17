89f92d5c1b6304ef160e1a51d979af72b4722560
soyiro <leiruzielrp@gmail.com>
2025-09-17 12:30:14 -0600

    Apis

--- js/fetch.js
+++ js/fetch.js
@@ -1,27 +1,49 @@
-const url_api = "https://pokeapi.co/api/v2/pokemon";
+const url_api = "https://fakestoreapi.com/products";
+
+const elementos2 = document.querySelector("#elementos2");
 
 fetch(url_api)
-  .then(respuesta => {
-    if (respuesta.ok) {
-      console.log(respuesta);
-      return respuesta.json();
+  .then(respuesta => respuesta.json()).then(datos => {
+    console.log(datos)
+  });
+
+
+function peticionAJAX(url) {
+  let xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      let datos = JSON.parse(xhr.responseText);
+      console.log(datos);
+      for (i = 0; i < datos.length; i++) {
+        console.log("Titulo: " + datos[i].title + "             Precio: " + datos[i].price);
+        
+        const tarjetacontenedor = document.createElement("div");
+        tarjetacontenedor.classList.add("tarjetacontenedor");
+        tarjetacontenedor.style.animationDelay = 0 + "s";
+        elementos2.prepend(tarjetacontenedor);
+
+        const imagen = document.createElement("img");
+        tarjetacontenedor.appendChild(imagen);
+        imagen.classList.add("imagen-nueva");
+        imagen.src = datos[i].image;
+
+        const texto = document.createElement("h2");
+        tarjetacontenedor.appendChild(texto);
+        texto.textContent = datos[i].title;
+
+        const parrafo = document.createElement("p");
+        tarjetacontenedor.appendChild(parrafo);
+        parrafo.textContent = datos[i].description;
+
+        const boton = document.createElement("button");
+        tarjetacontenedor.appendChild(boton);
+        boton.textContent = datos[i].price + "$";
+      }
     } else {
-      throw new Error("Error en la peticion: " + respuesta.status);
+      console.log("El error es: " + xhr.status);
     }
-  })
-    .then(datos => {
-    datos.results.forEach(pokemon => {
-      console.log(pokemon.name);
-      fetch(pokemon.url)
-        .then(res => {
-          if (res.ok) {
-            return res.json();
-          } else {
-            throw new Error(`Error al obtener datos de ${pokemon.name}`);
-          }
-        })
-        .then(datos_pokemon => {
-          console.log(datos_pokemon.name);
-        })
-    });
-  })
+  }
+  xhr.send();
+}
+peticionAJAX(url_api);
